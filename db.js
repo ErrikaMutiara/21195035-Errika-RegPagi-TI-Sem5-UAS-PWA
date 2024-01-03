eaa7f28a9ca5ba6c9f05f7738b4c4520f725335b
ErrikaMutiara <118159061+ErrikaMutiara@users.noreply.github.com>
2024-01-03 15:14:34 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,39 @@
+// Inisialisasi IndexedDB
+var db;
+var request = indexedDB.open('komentarDB', 1);
+
+request.onerror = function(event) {
+    console.log("Error saat membuka database: " + event.target.errorCode);
+};
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    var objectStore = db.createObjectStore("komentar", { keyPath: "id", autoIncrement: true });
+    objectStore.createIndex("name", "name", { unique: false });
+    objectStore.createIndex("email", "email", {unique: false});
+    objectStore.createIndex("message", "message", { unique: false });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    // showComments();
+};
+
+// Menambahkan komentar ke dalam IndexedDB
+document.getElementById('komentar').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    var transaction = db.transaction(['komentar'], 'readwrite');
+    var objectStore = transaction.objectStore('komentar');
+    var comment = { name: name, email : email, message: message };
+    objectStore.add(comment);
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+
+    // showComments();
+});
